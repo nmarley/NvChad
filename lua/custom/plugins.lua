5fbd66eb6e061cde6f9f604faf0a9dd4e59d1b41
Nathan Marley <nathan.marley@gmail.com>
2024-01-02 16:09:35 -0600

    Add copilot and shit

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -1,3 +1,5 @@
+local overrides = require("custom.configs.overrides")
+
 local plugins = {
   {
     "williamboman/mason.nvim",
@@ -15,6 +17,18 @@ local plugins = {
       require "plugins.configs.lspconfig"
       require "custom.configs.lspconfig"
     end,
+  },
+
+  {
+    "zbirenbaum/copilot.lua",
+    -- Lazy load when event occurs. Events are triggered
+    -- as mentioned in:
+    -- https://vi.stackexchange.com/a/4495/20389
+    event = "InsertEnter",
+    -- You can also have it load immediately at
+    -- startup by commenting above and uncommenting below:
+    -- lazy = false
+    opts = overrides.copilot,
   },
 }
 return plugins
