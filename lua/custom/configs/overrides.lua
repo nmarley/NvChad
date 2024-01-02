b04a02ed1dca5032c909daaf5fd7dce4157760e0
Nathan Marley <nathan.marley@gmail.com>
2024-01-02 16:09:46 -0600

    Overrides

---
+++ lua/custom/configs/overrides.lua
@@ -0,0 +1,11 @@
+local M = {}
+
+M.copilot = {
+  -- Possible configurable fields can be found on:
+  -- https://github.com/zbirenbaum/copilot.lua?tab=readme-ov-file#setup-and-configuration
+  suggestion = {
+    auto_trigger = true,
+  }
+}
+
+return M
