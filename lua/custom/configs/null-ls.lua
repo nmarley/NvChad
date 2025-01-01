981d7caa3165356adf4e07b94562025dbbd49215
Nathan Marley <nathan@blackcarrot.be>
2024-12-31 20:35:22 -0600

    Fix default JSON formatting w/o biome config

--- lua/custom/configs/null-ls.lua
+++ lua/custom/configs/null-ls.lua
@@ -3,7 +3,20 @@ local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 local opts = {
     sources = {
-        null_ls.builtins.formatting.biome,
+        -- null_ls.builtins.formatting.biome,
+        -- Run biome w/these extra args only if the biome.json file is not found
+        null_ls.builtins.formatting.biome.with({
+            condition = function(utils)
+                return not utils.root_has_file("biome.json")
+            end,
+            extra_args = { "--indent-width=2", "--indent-style=space" },
+        }),
+        -- ... otherwise run biome w/config rules defined in biome.json
+        null_ls.builtins.formatting.biome.with({
+            condition = function(utils)
+                return utils.root_has_file("biome.json")
+            end,
+        }),
     },
     on_attach = function(client, bufnr)
         if client.supports_method("textDocument/formatting") then
