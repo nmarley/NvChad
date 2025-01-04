d64e7c7d620de4d06be07ee8e6abc2a7ca558e56
Nathan Marley <nathan@blackcarrot.be>
2025-01-04 13:03:26 -0600

    Fix biome better

--- lua/custom/configs/null-ls.lua
+++ lua/custom/configs/null-ls.lua
@@ -7,14 +7,14 @@ local opts = {
         -- Run biome w/these extra args only if the biome.json file is not found
         null_ls.builtins.formatting.biome.with({
             condition = function(utils)
-                return not utils.root_has_file("biome.json")
+                return not utils.root_has_file("biome.json") and not utils.has_file("biome.json")
             end,
             extra_args = { "--indent-width=2", "--indent-style=space" },
         }),
         -- ... otherwise run biome w/config rules defined in biome.json
         null_ls.builtins.formatting.biome.with({
             condition = function(utils)
-                return utils.root_has_file("biome.json")
+                return utils.root_has_file("biome.json") or utils.has_file("biome.json")
             end,
         }),
     },
