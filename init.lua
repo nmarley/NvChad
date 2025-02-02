038266c92195fbc897a8f6690076b4e1cbdd3faf
Nathan Marley <nathan@glowberrylabs.com>
2025-02-02 10:01:53 -0600

    Remap F13 key to output nothing

--- init.lua
+++ init.lua
@@ -19,3 +19,15 @@ end
 dofile(vim.g.base46_cache .. "defaults")
 vim.opt.rtp:prepend(lazypath)
 require "plugins"
+
+-- Make vim stop outputting this:
+-- <F13><F13><F13><F13><F13><F13><F13><F13><F13><F13><F13><F13><F13>
+-- ... whenever I hold down <F13> key
+-- Normal mode mapping
+vim.keymap.set('n', '<F13>', '', { noremap = true, silent = true })
+-- Insert mode mapping
+vim.keymap.set('i', '<F13>', '', { noremap = true, silent = true })
+-- Visual mode mapping
+vim.keymap.set('v', '<F13>', '', { noremap = true, silent = true })
+-- Command-line mode mapping (if needed)
+vim.keymap.set('c', '<F13>', '', { noremap = true, silent = true })
