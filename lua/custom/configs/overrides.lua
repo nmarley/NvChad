a21d27a1acd85ef117204195268e7d7210f92777
Nathan Marley <nathan@glowberrylabs.com>
2025-08-24 16:24:49 -0500

    Restore copilot

--- lua/custom/configs/overrides.lua
+++ lua/custom/configs/overrides.lua
@@ -1,17 +1,17 @@
 local M = {}
 
--- M.copilot = {
---   -- Possible configurable fields can be found on:
---   -- https://github.com/zbirenbaum/copilot.lua?tab=readme-ov-file#setup-and-configuration
---   suggestion = {
---     auto_trigger = true,
---     keymap = {
---       accept = "<Tab>",  -- Replace <key combination> with your preferred keys
---     },
---   },
---   filetypes = {
---     markdown = true,
---   },
--- }
+M.copilot = {
+  -- Possible configurable fields can be found on:
+  -- https://github.com/zbirenbaum/copilot.lua?tab=readme-ov-file#setup-and-configuration
+  suggestion = {
+    auto_trigger = true,
+    keymap = {
+      accept = "<Tab>",  -- Replace <key combination> with your preferred keys
+    },
+  },
+  filetypes = {
+    markdown = true,
+  },
+}
 
 return M
--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -31,17 +31,17 @@ local plugins = {
     end,
   },
 
-  -- {
-  --   "zbirenbaum/copilot.lua",
-  --   -- Lazy load when event occurs. Events are triggered
-  --   -- as mentioned in:
-  --   -- https://vi.stackexchange.com/a/4495/20389
-  --   event = "InsertEnter",
-  --   -- You can also have it load immediately at
-  --   -- startup by commenting above and uncommenting below:
-  --   -- lazy = false
-  --   opts = overrides.copilot,
-  -- },
+  {
+    "zbirenbaum/copilot.lua",
+    -- Lazy load when event occurs. Events are triggered
+    -- as mentioned in:
+    -- https://vi.stackexchange.com/a/4495/20389
+    event = "InsertEnter",
+    -- You can also have it load immediately at
+    -- startup by commenting above and uncommenting below:
+    -- lazy = false
+    opts = overrides.copilot,
+  },
 
   -- 2024-02-27(nmarley): Add rust.vim plugin to autoformat code upon save
   {
